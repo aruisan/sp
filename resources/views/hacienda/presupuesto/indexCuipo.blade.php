60904c6d2c4b4266b25822bb0af030c8787d2c5f
unknown <deivith.1@hotmail.com>
2023-01-16 09:45:00 -0500

    reg

--- resources/views/hacienda/presupuesto/indexCuipo.blade.php
+++ resources/views/hacienda/presupuesto/indexCuipo.blade.php
@@ -405,7 +405,7 @@
                     <div id="tabReg" class=" tab-pane fade"><br>
                         <div class="table-responsive">
                             @if(count($registros) >= 1)
-                                @if(auth()->user()->id == 50 or auth()->user()->id == 45)
+                                @if(auth()->user()->id == 50 or auth()->user()->id == 45 or auth()->user()->roles->first()->id != 2)
                                     <a href="{{ url('administrativo/registros/'.$V) }}" class="btn btn-primary btn-block m-b-12">Ir a Registros</a>
                                 @endif
                                 <br><br>
