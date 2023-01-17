58c8a0e603b9bb9d0dd47e4ceafadf7a7d5b3429
unknown <deivith.1@hotmail.com>
2023-01-16 21:18:08 -0500

    fix

--- resources/views/hacienda/presupuesto/indexCuipo.blade.php
+++ resources/views/hacienda/presupuesto/indexCuipo.blade.php
@@ -838,7 +838,7 @@
                     <td>${e.cod_actividad}</td>
                     <td>${e.actividad}</td>
                     <td>${button}</td>
-                    <td><a href="/presupuesto/actividad/{{e.cod_actividad}}">{{e.id}}</a></td>
+                    <td><a href="/presupuesto/actividad/${e.cod_actividad}">${e}</a></td>
                 </tr>
             `);
             });
