eed1919e8bbb0b2947b88d71f524b190594c8766
Deivith Granados <deivith.1@hotmail.com>
2023-01-20 11:23:19 -0500

    pdf

--- resources/views/administrativo/ordenpagos/pdfCE.blade.php
+++ resources/views/administrativo/ordenpagos/pdfCE.blade.php
@@ -192,8 +192,24 @@
 							<td>{{$OrdenPago->pucs[$z]->data_puc->concepto}}</td>
 							<td>{{ $OrdenPago->registros->persona->num_dc }} {{ $OrdenPago->registros->persona->nombre }}</td>
 							<td>$<?php echo number_format($OrdenPago->pucs[$z]->valor_credito,0);?></td>
+							<td>$0</td>
 						</tr>
 					@endif
+				@endfor
+				@for($y = 0; $y < count($OrdenPago->pago->banks); $y++)
+					<tr class="text-center">
+						<td>{{ $OrdenPago->pago->banks[$y]->data_puc->code }}</td>
+						@if($OrdenPago->pago->type_pay == "ACCOUNT")
+							@php( $date = strftime("%d of %B %Y", strtotime($OrdenPago->pago->created_at)))
+							<td> Núm Cuenta: {{$OrdenPago->pago->num}} - Fecha: {{$date}}</td>
+						@elseif($OrdenPago->pago->type_pay == "CHEQUE")
+							@php( $date = strftime("%d of %B %Y", strtotime($OrdenPago->pago->created_at)))
+							<td> Núm Cheque: {{$OrdenPago->pago->num}} - Fecha: {{$date}}</td>
+						@endif
+						<td>{{ $OrdenPago->pago->banks[$y]->data_puc->concepto }}</td>
+						<td>0$</td>
+						<td>$<?php echo number_format($OrdenPago->pago->banks[$y]->valor,0);?></td>
+					</tr>
 				@endfor
 				</tbody>
 			</table>
