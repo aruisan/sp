55782712d020354b74dc13838606bab4953f2083
Deivith Granados <deivith.1@hotmail.com>
2023-01-26 23:05:42 -0500

    change

--- app/Http/Controllers/Administrativo/Contabilidad/LibrosController.php
+++ app/Http/Controllers/Administrativo/Contabilidad/LibrosController.php
@@ -74,11 +74,10 @@ class LibrosController extends Controller
                             if (Carbon::parse($pagoBank->created_at)->format('Y') == Carbon::today()->format('Y')) {
                                 $total = $total + $pagoBank->valor;
                                 $pago = Pagos::find($pagoBank->pagos_id);
-                                dd($pago,$pagoBank);
-                                $tercero = $pagoBank->pago->first()->orden_pago->registros->persona->nombre;
-                                $numIdent = $pagoBank->pago->first()->orden_pago->registros->persona->num_dc;
+                                $tercero = $pago->orden_pago->registros->persona->nombre;
+                                $numIdent = $pago->orden_pago->registros->persona->num_dc;
                                 $result[] = collect(['fecha' => Carbon::parse($pagoBank->created_at)->format('d-m-Y'), 'modulo' => 'Pago', 'debito' => '$'.number_format($pagoBank->valor,0),
-                                    'credito' => '$'.number_format(0,0), 'tercero' => $tercero, 'CC' => $numIdent, 'concepto' => $pagoBank->pago->first()->ordenPago->nombre, 'cuenta' => $rubroPUC->code.' - '.$rubroPUC->concepto,
+                                    'credito' => '$'.number_format(0,0), 'tercero' => $tercero, 'CC' => $numIdent, 'concepto' => $pago->concepto, 'cuenta' => $rubroPUC->code.' - '.$rubroPUC->concepto,
                                     'total' => '$'.number_format($total,0)]);
                             }
                         }
