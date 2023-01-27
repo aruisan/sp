7b23d0b151df734fed761ac416bd7a643788a41b
Deivith Granados <deivith.1@hotmail.com>
2023-01-26 22:25:56 -0500

    pagos en el libro contable

--- app/Http/Controllers/Administrativo/Contabilidad/LibrosController.php
+++ app/Http/Controllers/Administrativo/Contabilidad/LibrosController.php
@@ -7,6 +7,7 @@ use App\Model\Administrativo\Contabilidad\RegistersPuc;
 use App\Model\Administrativo\Contabilidad\RubrosPuc;
 use App\Http\Controllers\Controller;
 use App\Model\Administrativo\OrdenPago\OrdenPagosPuc;
+use App\Model\Administrativo\Pago\PagoBanks;
 use Illuminate\Http\Request;
 use Session;
 use Carbon\Carbon;
@@ -48,6 +49,8 @@ class LibrosController extends Controller
             $rubrosPUC = PucAlcaldia::where('padre_id',$item->id)->get();
             if ($rubrosPUC->count() >= 1){
                 foreach ($rubrosPUC as $rubroPUC){
+
+                    //SE AÑADEN LOS PALORES DE LAS ORDENES DE PAGO AL LIBRO
                     $ordenPagosPUC = OrdenPagosPuc::where('rubros_puc_id', $rubroPUC->id)->get();
                     if (count($ordenPagosPUC) > 0){
                         foreach ($ordenPagosPUC as $op_puc){
@@ -58,6 +61,22 @@ class LibrosController extends Controller
                                 $numIdent = $op_puc->ordenPago->registros->persona->num_dc;
                                 $result[] = collect(['fecha' => Carbon::parse($op_puc->created_at)->format('d-m-Y'), 'modulo' => 'Orden de Pago', 'debito' => '$'.number_format($op_puc->valor_debito,0),
                                     'credito' => '$'.number_format($op_puc->valor_credito,0), 'tercero' => $tercero, 'CC' => $numIdent, 'concepto' => $op_puc->ordenPago->nombre, 'cuenta' => $rubroPUC->code.' - '.$rubroPUC->concepto,
+                                    'total' => '$'.number_format($total,0)]);
+                            }
+                        }
+                    }
+
+                    // SE AÑADEN LOS VALORES DE LOS PAGOS AL LIBRO
+                    $pagoBanks = PagoBanks::where('rubros_puc_id', $rubroPUC->id)->get();
+                    if (count($pagoBanks) > 0){
+                        foreach ($pagoBanks as $pagoBank){
+                            if (Carbon::parse($pagoBank->created_at)->format('Y') == Carbon::today()->format('Y')) {
+                                $total = $total + $pagoBank->valor_debito;
+                                $total = $total - $pagoBank->valor_credito;
+                                $tercero = $pagoBanks->pago->orden_pago->registros->persona->nombre;
+                                $numIdent = $pagoBanks->pago->orden_pago->registros->persona->num_dc;
+                                $result[] = collect(['fecha' => Carbon::parse($pagoBank->created_at)->format('d-m-Y'), 'modulo' => 'Pago', 'debito' => '$'.number_format($pagoBank->valor_debito,0),
+                                    'credito' => '$'.number_format($pagoBank->valor_credito,0), 'tercero' => $tercero, 'CC' => $numIdent, 'concepto' => $pagoBank->ordenPago->nombre, 'cuenta' => $rubroPUC->code.' - '.$rubroPUC->concepto,
                                     'total' => '$'.number_format($total,0)]);
                             }
                         }
