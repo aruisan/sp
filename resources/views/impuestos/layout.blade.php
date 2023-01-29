576e1288e9e45c12c8f3c37a3cbe785f8d76d44a
unknown <deivith.1@hotmail.com>
2023-01-29 10:15:06 -0500

    arreglo title impuestos

--- resources/views/impuestos/layout.blade.php
+++ resources/views/impuestos/layout.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>IMPUESTOS - Concejo Providencia Islas</title>
+    <title>IMPUESTOS - Providencia Islas</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
