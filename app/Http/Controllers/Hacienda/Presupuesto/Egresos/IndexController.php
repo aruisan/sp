c6ef43860976826770c1dc7e3a72dedb0341eafc
Deivith Granados <deivith.1@hotmail.com>
2023-02-01 13:28:48 -0500

    fix

--- app/Http/Controllers/Hacienda/Presupuesto/Egresos/IndexController.php
+++ app/Http/Controllers/Hacienda/Presupuesto/Egresos/IndexController.php
@@ -698,7 +698,9 @@ class IndexController extends Controller
                                                                 foreach ($ordenPagoRubros as $ordenPagoRubro){
                                                                     if ($ordenPagoRubro->orden_pago->estado == 1 and $ordenPagoRubro->orden_pago->registros_id == $data->registro_id){
                                                                         $valueOrdenPago[] = $ordenPagoRubro->valor;
-                                                                        //if ($ordenPagoRubro->orden_pago->pago->estado == 1 )$valuePagos[] = $ordenPagoRubro->valor;
+                                                                        if ($ordenPagoRubro->orden_pago->pago){
+                                                                            if ($ordenPagoRubro->orden_pago->pago->estado == 1 ) $valuePagos[] = $ordenPagoRubro->valor;
+                                                                        }
                                                                     }
                                                                 }
                                                             }
