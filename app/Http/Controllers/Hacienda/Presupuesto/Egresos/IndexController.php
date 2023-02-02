ea96589767b0346ca8b6d2c18f3ed8e7dc151348
Deivith Granados <deivith.1@hotmail.com>
2023-02-01 22:20:49 -0500

    fix

--- app/Http/Controllers/Hacienda/Presupuesto/Egresos/IndexController.php
+++ app/Http/Controllers/Hacienda/Presupuesto/Egresos/IndexController.php
@@ -330,7 +330,7 @@ class IndexController extends Controller
                                 if(count($rubroOtherFind->first()->rubrosCdp) > 0){
                                     foreach ($rubroOtherFind->first()->rubrosCdp as $cdp) {
                                         if ($cdp->cdps->jefe_e == "3") {
-                                            $valueCDPs[] = $cdp->cdps->valor;
+                                            $valueCDPs[] = $cdp->valor;
                                             if (count($cdp->cdps->cdpsRegistro) > 0){
                                                 foreach ($cdp->cdps->cdpsRegistro as $cdpReg){
                                                     if ($cdpReg->registro->jefe_e == 3){
@@ -381,7 +381,7 @@ class IndexController extends Controller
 
                         //LLENADO DE PADRES
                         if (array_sum($valueRubros) > 0){
-                            //$presupuesto[] = ['id_rubro' => 0 ,'id' => $data->id, 'cod' => $data->code, 'name' => $data->name, 'presupuesto_inicial' => array_sum($valueRubros), 'adicion' => array_sum($valueRubrosAdd), 'reduccion' => array_sum($valueRubrosRed), 'credito' => array_sum($valueRubrosCred), 'ccredito' => array_sum($valueRubrosCCred), 'presupuesto_def' => $PDef, 'cdps' => array_sum($valueCDPs), 'registros' => array_sum($valueRegistros), 'saldo_disp' => $PDef - array_sum($valueCDPs), 'saldo_cdp' => array_sum($valueCDPs) - array_sum($valueRegistros), 'ordenes_pago' => array_sum($valueOrdenPago), 'pagos' => array_sum($valuePagos), 'cuentas_pagar' => array_sum($valueOrdenPago) - array_sum($valuePagos), 'reservas' => array_sum($valueRegistros) - array_sum($valueOrdenPago), 'rubros_disp' => 0, 'codBpin' => '', 'codActiv' => '', 'nameActiv' => '','codDep' => '', 'dep' => '', 'depRubID' => ''];
+                            $presupuesto[] = ['id_rubro' => 0 ,'id' => $data->id, 'cod' => $data->code, 'name' => $data->name, 'presupuesto_inicial' => array_sum($valueRubros), 'adicion' => array_sum($valueRubrosAdd), 'reduccion' => array_sum($valueRubrosRed), 'credito' => array_sum($valueRubrosCred), 'ccredito' => array_sum($valueRubrosCCred), 'presupuesto_def' => $PDef, 'cdps' => array_sum($valueCDPs), 'registros' => array_sum($valueRegistros), 'saldo_disp' => $PDef - array_sum($valueCDPs), 'saldo_cdp' => array_sum($valueCDPs) - array_sum($valueRegistros), 'ordenes_pago' => array_sum($valueOrdenPago), 'pagos' => array_sum($valuePagos), 'cuentas_pagar' => array_sum($valueOrdenPago) - array_sum($valuePagos), 'reservas' => array_sum($valueRegistros) - array_sum($valueOrdenPago), 'rubros_disp' => 0, 'codBpin' => '', 'codActiv' => '', 'nameActiv' => '','codDep' => '', 'dep' => '', 'depRubID' => ''];
                         }
 
 
