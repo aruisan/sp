2328ba127f805a08fe6c34cd9ff6620d2fa89ade
Deivith Granados <deivith.1@hotmail.com>
2023-02-05 13:10:25 -0500

    pago retefuente

--- app/Http/Controllers/Administrativo/OrdenPago/RetencionFuente/RetencionFuenteController.php
+++ app/Http/Controllers/Administrativo/OrdenPago/RetencionFuente/RetencionFuenteController.php
@@ -167,7 +167,6 @@ class RetencionFuenteController extends Controller
                         if ($ordenPago){
                             if ($ordenPago->registros->cdpsRegistro->first()->cdp->vigencia_id == $vigencia_id){
                                 //SE RECORRE EL PUC PARA OBTENER LOS VALORES DE LA OP
-                                dd($ordenPago->pucs);
                                 foreach ($ordenPago->pucs as $puc){
                                     //SE RECORRE EL PADRE CORRESPONDIENTE AL DEBITO PARA SABER SI UN HIJO CORRESPONDE
                                     if (count($hijosDeb) > 0){
