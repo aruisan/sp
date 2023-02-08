8f9b8b113c9400cb4a88d4740f988282362a183a
Deivith Granados <deivith.1@hotmail.com>
2023-02-07 20:40:26 -0500

    arreglo prep

--- app/Http/Controllers/Hacienda/Presupuesto/Egresos/IndexController.php
+++ app/Http/Controllers/Hacienda/Presupuesto/Egresos/IndexController.php
@@ -152,6 +152,23 @@ class IndexController extends Controller
                         $otherRubs = DB::select("SELECT * from plantilla_cuipos where code REGEXP CONCAT('^','".$data->code.".')");
                         foreach ($otherRubs as $other) {
                             $rubroOtherFind = Rubro::where('vigencia_id', $vigencia_id)->where('plantilla_cuipos_id', $other->id)->get();
+
+                            //CDPS
+                            $cdps = Cdp::where('vigencia_id', $vigencia_id)->where('jefe_e', '3')->get();
+                            if (count($cdps) > 0){
+                                $valueCDPs[] = $cdps->sum('valor');
+                            } else{
+                                $valueCDPs[] = 0;
+                            }
+
+                            $registros = Registro::where('id','>=', 778)->where('jefe_e','3')->get();
+                            if (count($registros) > 0){
+                                $valueRegistros[] = $registros->sum('valor');
+                            } else{
+                                $valueRegistros[] = 0;
+                            }
+
+
                             if($rubroOtherFind->first()) {
                                 //2 ADD - 3 RED  - 1 CRED
                                 if(count($rubroOtherFind->first()->rubrosMov) > 0){
@@ -172,21 +189,7 @@ class IndexController extends Controller
                                     $valueRubrosCCred[] = 0;
                                 }
 
-                                //CDPS
-                                $cdps = Cdp::where('vigencia_id', $vigencia_id)->where('jefe_e', '3')->get();
-                                if (count($cdps) > 0){
-                                    $valueCDPs[] = $cdps->sum('valor');
-                                } else{
-                                    $valueCDPs[] = 0;
-                                }
 
-                                $registros = Registro::where('id','>=', 778)->where('jefe_e','3')->get();
-                                if (count($registros) > 0){
-                                    $valueRegistros[] = $registros->sum('valor');
-                                } else{
-                                    $valueRegistros[] = 0;
-                                }
-                                
 
                                 //ORDENES DE PAGO
                                 if (isset($valores)){
