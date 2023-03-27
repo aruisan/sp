9b69345e13a4e58eb1ec37024112d7571543b05d
Deivith Granados <deivith.1@hotmail.com>
2023-03-27 11:24:52 -0500

    cdps

--- app/Http/Controllers/Administrativo/Cdp/CdpController.php
+++ app/Http/Controllers/Administrativo/Cdp/CdpController.php
@@ -415,7 +415,6 @@ class CdpController extends Controller
                             foreach($fuentesRubro->dependenciaFont as $dep){
                                 if($dep->dependencia_id == $update->dependencia_id){
                                     if ($dep->saldo < $data->rubrosCdpValor->first()->valor){
-                                        dd($dep, $data->rubrosCdpValor);
                                         Session::flash('success','El CDP enviado tiene asignado un valor superior al
                                             disponible en el rubro.');
                                         return back();
