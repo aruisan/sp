ffc0f73d4123058d9dc8301a9fd12da532d51439
Deivith Granados <deivith.1@hotmail.com>
2023-03-31 11:50:38 -0500

    librosController

--- app/Http/Controllers/Administrativo/Contabilidad/LibrosController.php
+++ app/Http/Controllers/Administrativo/Contabilidad/LibrosController.php
@@ -82,14 +82,21 @@ class LibrosController extends Controller
                         $ordenPagosPUC = OrdenPagosPuc::where('rubros_puc_id', $rubroPUC->id)->get();
                         if (count($ordenPagosPUC) > 0){
                             foreach ($ordenPagosPUC as $op_puc){
-                                if (Carbon::parse($op_puc->created_at)->format('Y') == Carbon::today()->format('Y')) {
-                                    $total = $total + $op_puc->valor_debito;
-                                    $total = $total - $op_puc->valor_credito;
-                                    $tercero = $op_puc->ordenPago->registros->persona->nombre;
-                                    $numIdent = $op_puc->ordenPago->registros->persona->num_dc;
-                                    $result[] = collect(['fecha' => Carbon::parse($op_puc->created_at)->format('d-m-Y'), 'modulo' => 'Orden de Pago', 'debito' => '$'.number_format($op_puc->valor_debito,0),
-                                        'credito' => '$'.number_format($op_puc->valor_credito,0), 'tercero' => $tercero, 'CC' => $numIdent, 'concepto' => $op_puc->ordenPago->nombre, 'cuenta' => $rubroPUC->code.' - '.$rubroPUC->concepto,
-                                        'total' => '$'.number_format($total,0)]);
+                                if ($op_puc->ordenPago->estado == 1){
+                                    if (Carbon::parse($op_puc->created_at)->format('Y') == Carbon::today()->format('Y')) {
+                                        $total = $total + $op_puc->valor_debito;
+                                        $total = $total - $op_puc->valor_credito;
+                                        if (isset($op_puc->ordenPago->registros->persona)){
+                                            $tercero = $op_puc->ordenPago->registros->persona->nombre;
+                                            $numIdent = $op_puc->ordenPago->registros->persona->num_dc;
+                                        } else{
+                                            $tercero = 'DIRECCIÓN DE IMPUESTOS Y ADUANAS DIAN';
+                                            $numIdent = 800197268;
+                                        }
+                                        $result[] = collect(['fecha' => Carbon::parse($op_puc->created_at)->format('d-m-Y'), 'modulo' => 'Orden de Pago', 'debito' => '$'.number_format($op_puc->valor_debito,0),
+                                            'credito' => '$'.number_format($op_puc->valor_credito,0), 'tercero' => $tercero, 'CC' => $numIdent, 'concepto' => $op_puc->ordenPago->nombre, 'cuenta' => $rubroPUC->code.' - '.$rubroPUC->concepto,
+                                            'total' => '$'.number_format($total,0)]);
+                                    }
                                 }
                             }
                         }
@@ -124,6 +131,7 @@ class LibrosController extends Controller
                             foreach ($compsCont as $compCont){
                                 if ($compCont->cuenta_banco == $rubroPUC->id or $compCont->cuenta_puc_id == $rubroPUC->id){
                                     $persona = Persona::find($compCont->comprobante->persona_id);
+                                    if (!isset($persona)) dd($compCont, $compCont->comprobante);
                                     $tercero = $persona->nombre;
                                     $numIdent = $persona->num_dc;
                                     if ($compCont->cuenta_banco == $rubroPUC->id){
