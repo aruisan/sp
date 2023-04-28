9508e364553ca11ea45113ca28e17d25e0b7ecc1
Deivith Granados <deivith.1@hotmail.com>
2023-04-28 10:17:10 -0500

    fix

--- app/Http/Controllers/Hacienda/Presupuesto/Informes/InformeDocsController.php
+++ app/Http/Controllers/Hacienda/Presupuesto/Informes/InformeDocsController.php
@@ -79,6 +79,7 @@ class InformeDocsController extends Controller
                     $ordenPagos[] = collect(['info' => $data, 'tercero' => $data->registros->persona->nombre,
                         'ccH' => $data->registros->persona->num_dc, 'descuentos' => $OrdenPagoDescuentos, 'pucs' => $data->pucs, 'pucV' => $data->pucs->sum('debito'),
                         'descV' => $OrdenPagoDescuentos->sum('valor')]);
+                    dd($ordenPagos, $OrdenPagoDescuentos, $data->pucs);
                 }
             } else{
                 $tesoreriaRetefuentePago = TesoreriaRetefuentePago::where('orden_pago_id', $data->id)->first();
