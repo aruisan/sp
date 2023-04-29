5192b1ed309c9ed342e81190843146307d233774
Deivith Granados <deivith.1@hotmail.com>
2023-04-28 21:17:24 -0500

    fix

--- resources/views/exports/infPagosExc.blade.php
+++ resources/views/exports/infPagosExc.blade.php
@@ -12,14 +12,26 @@
     </thead>
     <tbody>
         @foreach($pagos as $codigo)
-
+            @if(isset($codigo->cuentaOP))
+                @for($x = 0; $x < count($codigo->cuentaOP); $x++)
+                    <tr>
+                        <td>{{ $codigo['info']->ff_fin }}</td>
+                        <td>Pagos #{{ $codigo['info']->code }}</td>
+                        <td>{{ $codigo['info']->concepto }}</td>
+                        <td>{{ $codigo['info']->perOP[$x] }}</td>
+                        <td>{{ $codigo['info']->cuentaOP[$x] }} </td>
+                        <td>{{ $codigo['info']->credOP[$x] }}</td>
+                        <td>0</td>
+                    </tr>
+                @endfor
+            @endif
             <tr>
                 <td>{{ $codigo['info']->ff_fin }}</td>
                 <td>Pagos #{{ $codigo['info']->code }}</td>
                 <td>{{ $codigo['info']->concepto }}</td>
                 <td>{{ $codigo['info']->persona->num_dc }} - {{ $codigo['info']->persona->nombre }}</td>
                 <td>{{ $codigo['info']->cuentaBanco }} </td>
-                <td>{{ $codigo['info']->totCredOP }}</td>
+                <td>0</td>
                 <td>{{ $codigo['info']->valor }}</td>
             </tr>
         @endforeach
