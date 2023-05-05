d5babd2c36e9518bdfd1e81019f789bb2af83a41
Deivith Granados <deivith.1@hotmail.com>
2023-05-05 16:43:39 -0500

    fix

--- resources/views/impuestos/predial/pdf.blade.php
+++ resources/views/impuestos/predial/pdf.blade.php
@@ -27,12 +27,7 @@
                 <tr>
                     <td colspan="4">
                         Propietario(s):<br>
-                        <b>{{ $contribuyente->numIdent }} {{ $contribuyente->contribuyente }}</b><br>
-                        @if(count($contribuyentes) > 0)
-                            @foreach($contribuyentes as $otherProp)
-                                <b>{{$otherProp}}</b><br>
-                            @endforeach
-                        @endif
+                        <b>{{ $contribuyente->numIdent }} {{ $contribuyente->contribuyente }}</b>
                     </td>
                 </tr>
                 <tr>
