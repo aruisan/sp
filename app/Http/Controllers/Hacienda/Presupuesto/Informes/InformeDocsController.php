3d939b1b6ffee0f35b8ecb8b9e77295c6f924948
Deivith Granados <deivith.1@hotmail.com>
2023-05-05 13:44:47 -0500

    fix

--- app/Http/Controllers/Hacienda/Presupuesto/Informes/InformeDocsController.php
+++ app/Http/Controllers/Hacienda/Presupuesto/Informes/InformeDocsController.php
@@ -141,7 +141,7 @@ class InformeDocsController extends Controller
         $diaActual = Carbon::now()->day;
         $pagos = $this->generatePagos($añoActual);
 
-        dd($pagos);
+        dd($pagos[0]);
 
         return Excel::download(new InfPagosExcExport($pagos),
             'Informe de Pagos '.$añoActual.'-'.$mesActual.'-'.$diaActual.'.xlsx');
