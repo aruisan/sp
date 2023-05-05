5c04f0207c9c2819c325e6426b6a04c617b5739b
Deivith Granados <deivith.1@hotmail.com>
2023-05-04 21:54:32 -0500

    fix

--- app/Http/Controllers/Hacienda/Presupuesto/Informes/InformeDocsController.php
+++ app/Http/Controllers/Hacienda/Presupuesto/Informes/InformeDocsController.php
@@ -123,10 +123,10 @@ class InformeDocsController extends Controller
 
             foreach ($comprobante->movs as $movimiento){
                 if(isset($movimiento->cuenta_banco)){
-                    if (!isset($movimiento->banco->code)) dd($movimiento->banco, $comprobante->movs, $comprobante, 'BANK');
+                    if (!isset($movimiento->banco->code)) dd($movimiento, $movimiento->banco, $comprobante->movs, $comprobante, 'BANK');
                 }
                 if(isset($movimiento->cuenta_puc_id)){
-                    if (!isset($movimiento->puc->code)) dd($movimiento->puc, $comprobante->movs, $comprobante, 'PUC');
+                    if (!isset($movimiento->puc->code)) dd($movimiento, $movimiento->puc, $comprobante->movs, $comprobante, 'PUC');
                 }
             }
         }
