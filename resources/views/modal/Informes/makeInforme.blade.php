33387545987f2a6bf246996ed146653132197c61
Deivith Granados <deivith.1@hotmail.com>
2023-05-09 15:46:41 -0500

    fix

--- resources/views/modal/Informes/makeInforme.blade.php
+++ resources/views/modal/Informes/makeInforme.blade.php
@@ -14,6 +14,7 @@
                         <a class="btn-sm btn-primary" target="_blank" href="/presupuesto/informeGeneralIngresosPDF"> Ingresos <i class="fa fa-file-pdf-o "></i></a>
                         <a class="btn-sm btn-primary" target="_blank" href="/presupuesto/informeGeneralEgresosEXCEL"> Egresos <i class="fa fa-file-excel-o"></i></a>
                         <hr><h4>DOCUMENTOS</h4><hr>
+                        <a class="btn-sm btn-primary" target="_blank" href="/presupuesto/makeCDPsEXCEL"> CDPs <i class="fa fa-file-excel-o"></i></a>
                         <a class="btn-sm btn-primary" target="_blank" href="/presupuesto/makeOrdenPagosEXCEL"> Ordenes de Pago <i class="fa fa-file-excel-o"></i></a>
                         <a class="btn-sm btn-primary" target="_blank" href="/presupuesto/makePagosEXCEL"> Pagos <i class="fa fa-file-excel-o"></i></a>
                         <a class="btn-sm btn-primary" target="_blank" href="/presupuesto/makeCCEXCEL"> Comprobantes Contabilidad <i class="fa fa-file-excel-o"></i></a>
