bcb5697749bc863d40611c16da7cb0ee52f9f275
unknown <deivith.1@hotmail.com>
2023-07-13 15:08:07 -0500

    chip

--- resources/views/modal/Informes/makeCHIP.blade.php
+++ resources/views/modal/Informes/makeCHIP.blade.php
@@ -46,6 +46,7 @@
                             </div>
                             <div class="form-group row">
                                 <div class="col-lg-12 ml-auto text-center">
+                                    <br><br><br>    
                                     <button type="submit" class="btn btn-primary">Generar CHIP</button>
                                 </div>
                             </div>
