926c4e70820fd7885e9211128b73523cdd811743
Deivith Granados <deivith.1@hotmail.com>
2023-07-19 10:06:17 -0500

    fix

--- resources/views/administrativo/pagos/createBanks.blade.php
+++ resources/views/administrativo/pagos/createBanks.blade.php
@@ -27,8 +27,8 @@
                 </div>
                 <div class="col-md-4 text-center">
                     <b>Monto a Pagar:
-                        <input type="hidden" id="montoPago" value="{{ $pago->orden_pago->saldo }}">
-                        <span id="montoPagoSpan">$<?php echo number_format( $pago->orden_pago->saldo,0) ?></span></b>
+                        <input type="hidden" id="montoPago" value="{{ $pago->valor }}">
+                        <span id="montoPagoSpan">$<?php echo number_format( $pago->valor,0) ?></span></b>
                 </div>
                 <div class="col-md-4 text-center">
                     @if(isset($pago->orden_pago->registros))
