4a2f839a8309f4ce09e863c0bed2ecbe15410543
Deivith Granados <deivith.1@hotmail.com>
2023-09-11 19:06:23 -0500

    fix

--- app/Console/Commands/feedPresupuesto.php
+++ app/Console/Commands/feedPresupuesto.php
@@ -54,7 +54,7 @@ class feedPresupuesto extends Command
             if ($registro->ordenPagos->count() > 0){
                 $disp = $registro->valor - $registro->ordenPagos->where('estado','1')->sum('valor');
                 if ($disp != $registro->saldo){
-                    dd($registro);
+                    echo($registro->id+'<br>');
                 }
             }
         }
