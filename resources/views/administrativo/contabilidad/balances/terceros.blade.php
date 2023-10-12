b33d58fe6247ebf300ba2e58c125ecf1ebb05b3f
Deivith Granados <deivith.1@hotmail.com>
2023-10-12 09:02:39 -0500

    fix

--- resources/views/administrativo/contabilidad/balances/terceros.blade.php
+++ resources/views/administrativo/contabilidad/balances/terceros.blade.php
@@ -31,28 +31,26 @@
                     </b></h4>
             </strong>
         </div>
-        <div class="table-responsive">
-            <div class="text-center" id="cargando" style="display: none">
-                <br><br>
-                <h4>Cargando balance...</h4>
-            </div>
-            <table style="display: none" class="table table-bordered table-hover" id="tabla">
-                <hr>
-                <thead>
-                <tr><th colspan="7" class="text-center"> <span id="cuentaBanco"></span></th></tr>
-                <tr>
-                    <th class="text-center">Fecha</th>
-                    <th class="text-center">Cuenta</th>
-                    <th class="text-center">Nombre Documento</th>
-                    <th class="text-center">Concepto</th>
-                    <th class="text-center">Debito</th>
-                    <th class="text-center">Credito</th>
-                    <th class="text-center">Tercero</th>
-                </tr>
-                </thead>
-                <tbody id="bodyTabla"></tbody>
-            </table>
+        <div class="text-center" id="cargando" style="display: none">
+            <br><br>
+            <h4>Cargando balance...</h4>
         </div>
+        <table style="display: none" class="table table-bordered table-hover" id="tabla">
+            <hr>
+            <thead>
+            <tr><th colspan="7" class="text-center"> <span id="cuentaBanco"></span></th></tr>
+            <tr>
+                <th class="text-center">Fecha</th>
+                <th class="text-center">Cuenta</th>
+                <th class="text-center">Nombre Documento</th>
+                <th class="text-center">Concepto</th>
+                <th class="text-center">Debito</th>
+                <th class="text-center">Credito</th>
+                <th class="text-center">Tercero</th>
+            </tr>
+            </thead>
+            <tbody id="bodyTabla"></tbody>
+        </table>
     </div>
 @stop
 
