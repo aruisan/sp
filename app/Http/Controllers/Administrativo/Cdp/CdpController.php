7ef8309876a019d582365d43f23bdd92476249e0
Deivith Granados <deivith.1@hotmail.com>
2023-10-13 10:16:30 -0500

    to 29 Sep

--- app/Http/Controllers/Administrativo/Cdp/CdpController.php
+++ app/Http/Controllers/Administrativo/Cdp/CdpController.php
@@ -38,7 +38,7 @@ class CdpController extends Controller
 
     public function __construct()
     {
-        $this->fechaFija = '2023-09-28';
+        $this->fechaFija = '2023-09-29';
     }
 
     /**
