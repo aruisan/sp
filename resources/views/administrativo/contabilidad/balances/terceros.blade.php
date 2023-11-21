7bdc824f5b4bd633ace48c3c63058e3d48dceab1
Deivith Granados <deivith.1@hotmail.com>
2023-11-21 13:26:23 -0500

    fix

--- resources/views/administrativo/contabilidad/balances/terceros.blade.php
+++ resources/views/administrativo/contabilidad/balances/terceros.blade.php
@@ -20,8 +20,8 @@
                         <option value="7/7">Julio</option>
                         <option value="8/8">Agosto</option>
                         <option value="9/9">Septiembre</option>
-                        <option disabled value="10/10">Octubre</option>
-                        <option disabled value="11/11">Noviembre</option>
+                        <option value="10/10">Octubre</option>
+                        <option value="11/11">Noviembre</option>
                         <option disabled value="12/12">Diciembre</option>
                         <option value="1/3">Enero - Marzo</option>
                         <option value="1/6">Enero - Junio</option>
