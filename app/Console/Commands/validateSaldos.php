2d2e3169c5a13dfd287be9251851eeb262e1ef85
Deivith Granados <deivith.1@hotmail.com>
2023-11-21 18:35:14 -0500

    fix

--- app/Console/Commands/validateSaldos.php
+++ app/Console/Commands/validateSaldos.php
@@ -51,7 +51,7 @@ class validateSaldos extends Command
         foreach ($allAccounts as $cuenta){
             $librosTraits = new LibrosTraits();
             $resultFind = $librosTraits->validateBeforeMonths($añoActual.'-12-31', $cuenta);
-            dd($resultFind);
+            dd($resultFind, $cuenta);
             if (count($resultFind) > 0){
                 $cuenta->saldo_actual = intval(str_replace(array("$", ","),'', $resultFind[count($resultFind) - 1]['total']));
                 $cuenta->save();
