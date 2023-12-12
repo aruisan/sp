089e9457da96286955e5ddac65c849a8838cf118
Deivith Granados <deivith.1@hotmail.com>
2023-12-12 17:59:51 -0500

    fix

---
+++ app/Console/Commands/validateSaldosRubros.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\Administrativo\Contabilidad\PucAlcaldia;
+use App\Model\Administrativo\Registro\Registro;
+use App\Model\Hacienda\Presupuesto\Rubro;
+use App\Model\Hacienda\Presupuesto\Snap\PresupuestoSnapData;
+use App\Model\Hacienda\Presupuesto\Snap\PresupuestoSnap;
+use App\Model\Hacienda\Presupuesto\Vigencia;
+use App\Traits\LibrosTraits;
+use App\Traits\PrepEgresosTraits;
+use App\Traits\PrepIngresosTraits;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+
+class validateSaldosRubros extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'validate:saldosRubros';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Valida los saldos de los Rubros y Actividades';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $año = Carbon::today()->year;
+        $vigens = Vigencia::where('vigencia', $año)->where('tipo', 0)->where('estado', '0')->first();
+        $rubros = Rubro::where('vigencia_id', $vigens->id)->get();
+        dd($rubros);
+    }
+}
