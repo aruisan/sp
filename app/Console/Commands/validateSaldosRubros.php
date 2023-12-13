8002f3e4aa2f315f4c45e2e0657726799a2580c9
Deivith Granados <deivith.1@hotmail.com>
2023-12-12 19:06:49 -0500

    fix

--- app/Console/Commands/validateSaldosRubros.php
+++ app/Console/Commands/validateSaldosRubros.php
@@ -75,7 +75,7 @@ class validateSaldosRubros extends Command
                     $actividad->propios = $actividad->propios - $ccred->sum('valor');
 
                     if ($actividad->propios - array_sum($valueCdps) != $actividad->saldo){
-                        dd($actividad, array_sum($valueCdps), $actividad->bpin);
+                        dd($actividad, array_sum($valueCdps), $actividad->bpin->cod_actividad);
                     } else unset($valueCdps);
                 }
             }
