f8fe035532d701da9838812ba757a677a269b3bc
unknown <deivith.1@hotmail.com>
2024-01-01 16:29:28 -0500

    fix

--- app/Console/Commands/feedPresupuesto.php
+++ app/Console/Commands/feedPresupuesto.php
@@ -44,8 +44,8 @@ class feedPresupuesto extends Command
     public function handle()
     {
         //EGRESOS
-        $añoActual = 2023;
-        $mesActual = 12;
+        $añoActual = Carbon::now()->year;
+        $mesActual = Carbon::now()->month;
 
         $vigens = Vigencia::where('vigencia', $añoActual)->where('tipo', 0)->where('estado', '0')->first();
 
